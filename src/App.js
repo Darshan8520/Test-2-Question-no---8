5c726af1a89ad899ed098ade4ba62d7693e3bf25
Darshan8520 <panchaldarshan4033@gmail.com>
2023-04-18 12:46:50 +0530

    Question -8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((resp) => {
+        setData(resp.data)
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        data.map((item) => {
+          console.log(item)
+          return (
+            <div>
+              <div>{item.id}</div>
+              <div>{item.userId}</div>
+              <div>{item.body}</div>
+              <div>{item.title}</div>
+            </div>
+          )
+        })
+      }
+    </>
   );
 }
 
